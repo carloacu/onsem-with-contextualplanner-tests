440342559cbaabfbef606dbbdf9feb65f34fde7d
carloacu <carloacu.dev@gmail.com>
2022-04-04 19:34:52 +0200

    Add submodules, voicegui and tests folder

---
+++ onsem-with-contextualplanner-tests-config.cmake
@@ -0,0 +1,6 @@
+get_filename_component(_onsem_contextualplanner_tests_root "${CMAKE_CURRENT_LIST_FILE}" PATH)
+get_filename_component(_onsem_contextualplanner_tests_root "${onsem_contextualplanner_tests_root}" ABSOLUTE)
+
+include(${_onsem_contextualplanner_tests_root}/contextualplanner/contextualplanner-config.cmake)
+include(${_onsem_contextualplanner_tests_root}/onsem/onsem-config.cmake)
+
